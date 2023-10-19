f1c798c33a4c7dec2f93922c944a5627deaecb44
Maple.Wu <lingfeng.wu@mihoyo.com>
2023-10-19 22:12:51 +0800

    feat(doc): update

--- api02/zzgen.api.go
+++ api02/zzgen.api.go
@@ -40,7 +40,9 @@ func (s Apis) _T() (interface{}, []map[string]interface{}) {
 			"options": map[string]string{
 				"path": "ret",
 			},
-			"invoke": func(ctx context.Context, dec func(interface{}) error) (interface{}, error) { return t.Ret(), nil },
+			"invoke": func(ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+				return t.Ret(), nil
+			},
 		},
 		{
 			"name":     "Error",
@@ -48,7 +50,9 @@ func (s Apis) _T() (interface{}, []map[string]interface{}) {
 			"options": map[string]string{
 				"path": "error",
 			},
-			"invoke": func(ctx context.Context, dec func(interface{}) error) (interface{}, error) { return nil, t.Error() },
+			"invoke": func(ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+				return nil, t.Error()
+			},
 		},
 		{
 			"name":     "RetError",
@@ -56,7 +60,9 @@ func (s Apis) _T() (interface{}, []map[string]interface{}) {
 			"options": map[string]string{
 				"path": "ret_error",
 			},
-			"invoke": func(ctx context.Context, dec func(interface{}) error) (interface{}, error) { return t.RetError() },
+			"invoke": func(ctx context.Context, dec func(interface{}) error) (interface{}, error) {
+				return t.RetError()
+			},
 		},
 		{
 			"name":     "Context",
